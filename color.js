c7cc51aac38f9b0ac3edff68cc37c5febf1153f6
Quinn Zepeda <developerquinnz@gmail.com>
2017-03-15 16:31:04 -0700

    winning changes banner color, starting new game resets background to origional color

--- color.js
+++ color.js
@@ -1,9 +1,25 @@
 var colors = generateRandomColors(6);
-
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");
+var resetButton = document.getElementById("reset");
+
+resetButton.addEventListener("click", function() {
+	colors = generateRandomColors(6);
+
+	pickedColor = pickColor();
+
+	colorDisplay.textContent = pickedColor;
+
+	for(var i = 0; i < squares.length; i++) {
+	// add initial colors
+	squares[i].style.background = colors[i];
+
+  }
+  h1.style.background = "#232323";
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -14,10 +30,11 @@ for(var i = 0; i < squares.length; i++) {
 	//add click listener to squares
 	squares[i].addEventListener("click", function () {
 		var clickedColor = this.style.background;
-			console.log(clickedColor, pickedColor)
 		if(clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct";
+			resetButton.textContent = "Play Again?";
 			changeColors(clickedColor);
+			h1.style.background = clickedColor;
 		} else {
 			this.style.background = "#232323";
 			messageDisplay.textContent = "Try Again"
